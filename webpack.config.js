e8caf46b969a37c36c0f843a6ef9ec4cd1aca5eb
ryantohhr <ryantohhongrui@hotmail.com>
2024-11-12 18:03:43 +0800

    Completed HTML initial design

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,29 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/index.html"],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|avif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
 };
