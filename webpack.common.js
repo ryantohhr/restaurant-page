3f6b40e4610818711326582affcec6a72908ec1c
ryantohhr <ryantohhongrui@hotmail.com>
2024-11-13 20:52:42 +0800

    Converted single webpack config file to have both dev and prod versions

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,18 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/index.html"],
-    },
+    entry: './src/index.js',
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
         }),
     ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
     module: {
         rules: [
             {
